d99677e4d1685b7fe40cfd6b9a3f31020057cc53
lin <linxan123@gmail.com>
2016-02-21 21:49:18 +0800

    添加了StartScene
    读取cocosstudio发布的资源
    按钮回调切换场景

---
+++ Classes/StartScene.cpp
@@ -0,0 +1,65 @@
+#include "StartScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+using namespace ui;
+
+Scene* StartScene::createScene()
+{
+	// 'scene' is an autorelease object
+	auto scene = Scene::create();
+
+	// 'layer' is an autorelease object
+	auto layer = StartScene::create();
+
+	// add layer as a child to scene
+	scene->addChild(layer);
+
+	// return the scene
+	return scene;
+}
+
+bool StartScene::init()
+{
+	if (!Layer::init())
+	{
+		return false;
+	}
+
+	
+
+	auto rootNode = CSLoader::createNode("StartScene.csb");
+	addChild(rootNode);
+
+	Button* startBtn = (Button*)Helper::seekWidgetByName((Widget*)rootNode,"StartBtn");
+	startBtn->addTouchEventListener(CC_CALLBACK_2(StartScene::touchEvent, this));
+	
+
+	return true;
+}
+
+void StartScene::touchEvent(cocos2d::Ref* sender, cocos2d::ui::Widget::TouchEventType type)
+{
+	auto director = Director::getInstance();
+	auto scene = StartScene::createScene();
+	scene = TransitionFade::create(1.0f, scene, Color3B::WHITE);
+	switch (type)
+	{
+	case cocos2d::ui::Widget::TouchEventType::BEGAN:
+		
+		break;
+	case cocos2d::ui::Widget::TouchEventType::MOVED:
+		break;
+	case cocos2d::ui::Widget::TouchEventType::ENDED:
+		director->replaceScene(scene);
+		break;
+	case cocos2d::ui::Widget::TouchEventType::CANCELED:
+		break;
+	default:
+		break;
+	}
+}
---
+++ Classes/StartScene.h
@@ -0,0 +1,21 @@
+#ifndef __START_SCENE_H__
+#define __START_SCENE_H__
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+class StartScene:public cocos2d::Layer
+{
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	CREATE_FUNC(StartScene);
+
+	void touchEvent(cocos2d::Ref* sender, cocos2d::ui::Widget::TouchEventType type);
+protected:
+private:
+};
+#endif 
+
+
